93715d96dfadd61707f29ccf2aafdf9ac8416f3f
JayKothari <jaykothari49@gmail.com>
2022-05-27 13:49:59 +0530

    scrap complete

---
+++ Pharma/spiders/Med.py
@@ -0,0 +1,43 @@
+import scrapy
+import requests
+from bs4 import BeautifulSoup
+
+class MedSpider(scrapy.Spider):
+    name = 'Med'
+    allowed_domains = ['mcareexports.com']
+    start_urls = ['http://mcareexports.com/our-products/']
+    page = 2
+
+    def parse(self, response):
+
+        tags = response.css('span.nxowoo-box')
+
+        for one in tags:
+            title = one.css('h2.woocommerce-loop-product__title::text')[0].extract()
+            img = one.css('a.woocommerce-LoopProduct-link img::attr(data-src)')[0].extract()
+            # med_name = '-'.join(title.split())
+            # url = f'https://mcareexports.com/our-products/{med_name}/'
+            # res = requests.get(url)
+            # mg = self.inside(res.text)
+            yield {
+                'title': title,
+                'img':img,
+                # 'mg':mg
+                }
+
+        if self.page < 3:
+            next_url = f'https://mcareexports.com/our-products/page/{self.page}/'
+            print(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Page : {self.page} <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+
+            self.page += 1
+            yield response.follow(next_url,callback = self.parse)
+
+    def inside(self,response):
+        soup = BeautifulSoup(response,'html.parser')
+        try:
+            tag = soup.find("div",class_='woocommerce-product-details__short-description')
+            p = tag.find_all('p')[-1]
+            return p.text
+        except:
+            return None
+        
